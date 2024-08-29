c42f72992f2789f5d8506b7853c83ed43238c308
Cole Naidoo <ST10283122@vcconnect.edu.za>
2024-08-29 13:52:01 +0200

    Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Storage.Queues;
+using System;
+
+namespace FibonacciSequenceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connectionString = "Your_Azure_Queue_Storage_Connection_String";
+            string queueName = "fibonaccisequence";
+
+
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            queueClient.CreateIfNotExists();
+
+            int a = 0, b = 1;
+            while (a <= 233)
+            {
+                Console.WriteLine(a);
+
+                queueClient.SendMessage(a.ToString());
+
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            Console.WriteLine("Fibonacci sequence generated and stored in Azure Queue Storage.");
+        }
+    }
+}
